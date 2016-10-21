7112ecdaed958715c2d0da6fc7fe8d4df2e8c168
JackAbel <JinXB2015@outlook.com>
2016-10-21 10:34:40 +0800

    commit hello world!

---
+++ src/org/Jack/Hello.java
@@ -0,0 +1,10 @@
+package org.Jack;
+
+/**
+ * Created by Jack on 2016/10/21.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
